208cca47ee313a85438bc21910b130f8dd88d8a4
simon.ferguson <simon.ferguson@kineticit.com.au>
2023-10-01 16:02:13 +0800

    added logging

--- fuckedge.ps1
+++ fuckedge.ps1
@@ -1,10 +1,25 @@
+$Logfile = ".\fuckedge.log"
+
+function WriteLog
+{
+    Param ([string]$LogString)
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $LogMessage = "$Stamp $LogString"
+    Add-content $LogFile -value $LogMessage
+}
 while($true)
 {
-    get-process | Where-Object ProcessName -Like "*msedge*" | stop-process
+    if ((get-process | Where-Object ProcessName -Like "*msedge*" ).count -gt 0 )
+    {
+        get-process | Where-Object ProcessName -Like "*msedge*" | stop-process
+        WriteLog("Killed Edge")
+    }
+
 
     if ((get-process | Where-Object { $_.ProcessName -Like "*Diablo*" -or $_.ProcessName -like "*Cyber*"}).count -ne 0){
         if (-not (Get-MpPreference).DisableRealtimeMonitoring){
             Set-MpPreference -DisableRealtimeMonitoring $true
+            WriteLog("Disabled AV")
         }
     }
     else {

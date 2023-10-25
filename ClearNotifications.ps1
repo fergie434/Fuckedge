bb4872c7202232e6f451d564b4e0c976337100ba
simon.ferguson <simon.ferguson@kineticit.com.au>
2023-10-25 20:36:39 +0800

    Improved to loop

--- ClearNotifications.ps1
+++ ClearNotifications.ps1
@@ -4,10 +4,19 @@
 $notifications = Get-ChildItem HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings | Select-Object Name
 
 # iterate through the keys, extract the name that will be used in the clear function, and clear the notifications
-for ($index = 0; $index -lt $notifications.Count; $index++) {
-    $name = $notifications[$index]
-    $split = $name -split "\\"
-    $last = $split[$split.Count - 1]
-    $last = $last.Substring(0, $last.Length - 1)
-    ([Windows.UI.Notifications.ToastNotificationManager]::History).clear($last)
+while ($true){
+    for ($index = 0; $index -lt $notifications.Count; $index++) {
+        $name = $notifications[$index]
+        $split = $name -split "\\"
+        $last = $split[$split.Count - 1]
+        $last = $last.Substring(0, $last.Length - 1)
+        $History = ([Windows.UI.Notifications.ToastNotificationManager]::History).GetHistory($last)
+        $Count = ($History | Measure).Count
+    
+        if ($Count){
+            ([Windows.UI.Notifications.ToastNotificationManager]::History).clear($last)
+            write-host "Cleared $($last) Notifications"
+        }
+    }
+    Start-Sleep -Seconds 3
 }
